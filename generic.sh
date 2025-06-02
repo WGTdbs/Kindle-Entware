d304b0550663142263f5d14abdcf38471774538d
WGTdbs <wuguantingdanbishi@outlook.com>
2025-06-02 20:24:26 +0800

    Update generic.sh

--- generic.sh
+++ generic.sh
@@ -17,58 +17,59 @@ LOADER=ld-linux.so.3
 GLIBC=2.23
 
 echo 'Info: Checking for prerequisites and creating folders...'
-if [ -d /mnt/us/opt ]; then
-    echo 'Warning: Folder /mnt/us/opt exists!'
+if [ -d /opt ]; then
+    echo 'Warning: Folder /opt exists!'
 else
-    mkdir /mnt/us/opt
+    mkdir /opt
 fi
 # no need to create many folders. entware-opt package creates most
 for folder in bin etc lib/opkg tmp var/lock
 do
-  if [ -d "/mnt/us/opt/$folder" ]; then
-    echo "Warning: Folder /mnt/us/opt/$folder exists!"
-    echo 'Warning: If something goes wrong please clean /mnt/us/opt folder and try again.'
+  if [ -d "/opt/$folder" ]; then
+    echo "Warning: Folder /opt/$folder exists!"
+    echo 'Warning: If something goes wrong please clean /opt folder and try again.'
   else
-    mkdir -p /mnt/us/opt/$folder
+    mkdir -p /opt/$folder
   fi
 done
 
 echo 'Info: Opkg package manager deployment...'
-URL=http://bin.entware.net/ ${ARCH}/installer
-wget $URL/opkg -O /mnt/us/opt/bin/opkg
-chmod 755 /mnt/us/opt/bin/opkg
-wget $URL/opkg.conf -O /mnt/us/opt/etc/opkg.conf
+URL=http://bin.entware.net/${ARCH}/installer
+wget $URL/opkg -O /opt/bin/opkg
+chmod 755 /opt/bin/opkg
+wget $URL/opkg.conf -O /opt/etc/opkg.conf
 
 echo 'Info: Basic packages installation...'
-/mnt/us/opt/bin/opkg update
+/opt/bin/opkg update
 if [ $TYPE = 'alternative' ]; then
-  /mnt/us/opt/bin/opkg install busybox
+  /opt/bin/opkg install busybox
 fi
-/mnt/us/opt/bin/opkg install entware-opt
+/opt/bin/opkg install entware-opt
 
 # Fix for multiuser environment
-chmod 777 /mnt/us/opt/tmp
+chmod 777 /opt/tmp
 
 for file in passwd group shells shadow gshadow; do
   if [ $TYPE = 'generic' ]; then
     if [ -f /etc/$file ]; then
-      ln -sf /etc/$file /mnt/us/opt/etc/$file
+      ln -sf /etc/$file /opt/etc/$file
     else
-      [ -f /mnt/us/opt/etc/$file.1 ] && cp /mnt/us/opt/etc/$file.1 /mnt/us/opt/etc/$file
+      [ -f /opt/etc/$file.1 ] && cp /opt/etc/$file.1 /opt/etc/$file
     fi
   else
-    if [ -f /mnt/us/opt/etc/$file.1 ]; then
-      cp /mnt/us/opt/etc/$file.1 /mnt/us/opt/etc/$file
+    if [ -f /opt/etc/$file.1 ]; then
+      cp /opt/etc/$file.1 /opt/etc/$file
     fi
   fi
 done
 
-[ -f /etc/localtime ] && ln -sf /etc/localtime /mnt/us/opt/etc/localtime
+[ -f /etc/localtime ] && ln -sf /etc/localtime /opt/etc/localtime
+
 
 echo 'Info: Finish!'
 echo 'Info: Kindle-Entware was successfully initialized.'
-echo 'Info: Add /mnt/us/opt/bin & /mnt/us/opt/sbin to $PATH variable'
-echo 'Info: Add "/mnt/us/opt/etc/init.d/rc.unslung start" to startup script for Kindle-Entware services to start'
+echo 'Info: Add opt/bin & opt/sbin to $PATH variable'
+echo 'Info: Add "opt/etc/init.d/rc.unslung start" to startup script for Kindle-Entware services to start'
 
 echo 'Info: Found a Bug? Please report at wuguantingdanbishi@outlook.com'
 echo 'Follow my BiliBili:wuguantingdanbish'
